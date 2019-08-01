e64520ebc0cdffc9fc9a1722e045c2d0fd75e8e3
Shivani Singh <52009090+shivihvs@users.noreply.github.com>
2019-08-01 21:58:31 +0530

    Add files via upload

---
+++ printing patterns in c.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n,i,j,min=0,k;
+    int a[4];
+    scanf("%d", &n);
+    for(i=0;i<2*n-1;++i)
+    {
+        for(j=0;j<2*n-1;++j)
+        { min=0;
+    
+          a[0]=i;
+          a[1]=j;
+          a[2]=2*n-2-i;
+          a[3]=2*n-2-j;
+          min=a[0];
+          for(k=1;k<4;k++)
+          {
+              if(min>a[k])
+              {
+                  min=a[k];
+              }
+          }
+          printf("%d ",n-min);
+
+        }
+        printf("\n");
+    }
+  	// Complete the code to print the pattern.
+    return 0;
+}
+
+
